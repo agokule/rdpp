edaa2948e4c59f343d98a8c65d06bef2f975d75d
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-10-12 18:27:36 -0400

    got a basic program running

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,7 +2,96 @@
 #include <format>
 #include <string>
 
-int main() {
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
+
+struct AppState {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    ImVec4 color;
+};
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     std::string message = std::format("The answer is {}", 42);
     std::cout << message << std::endl;
+
+    AppState *state = new AppState;
+
+    /* Create the window */
+    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE, &state->window, &state->renderer)) {
+        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL3_InitForSDLRenderer(state->window, state->renderer);
+    ImGui_ImplSDLRenderer3_Init(state->renderer);
+
+    *appstate = state;
+
+    return SDL_APP_CONTINUE;
 }
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    AppState &state = *static_cast<AppState*>(appstate);
+
+    if (event->type == SDL_EVENT_QUIT)
+        return SDL_APP_SUCCESS;
+
+    ImGui_ImplSDL3_ProcessEvent(event);
+    if (ImGui::GetIO().WantCaptureMouse) return SDL_APP_CONTINUE;
+    if (ImGui::GetIO().WantCaptureKeyboard) return SDL_APP_CONTINUE;
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    AppState &state = *static_cast<AppState*>(appstate);
+    SDL_Renderer *renderer = state.renderer;
+
+    ImGui_ImplSDL3_NewFrame();
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui::NewFrame();
+
+    auto color_raw = ImGui::ColorConvertFloat4ToU32(state.color);
+    uint8_t* color = reinterpret_cast<uint8_t*>(&color_raw);
+
+    std::cout << std::format("Rendering color: ({}, {}, {}, {})", color[0], color[1], color[2], color[3]) << std::endl;
+
+    SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], color[3]);
+    SDL_RenderClear(renderer);
+
+    ImGui::Begin("Hello");
+
+    ImGui::ColorPicker4("Choose a Color", &state.color.x, ImGuiColorEditFlags_Uint8);
+    ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    AppState *state = static_cast<AppState *>(appstate);
+    ImGui_ImplSDL3_Shutdown();
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui::DestroyContext();
+    SDL_DestroyRenderer(state->renderer);
+    SDL_DestroyWindow(state->window);
+    delete state;
+}
+
